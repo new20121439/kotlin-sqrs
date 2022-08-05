4a366fcc6a88f005fb58eec891a155632e5a6207
new20121439 <tranvandung727602@gmail.com>
2022-08-05 23:40:39 +0700

    Add some tests

--- src/test/kotlin/com/deep/escqrs/ProductTests.kt
+++ src/test/kotlin/com/deep/escqrs/ProductTests.kt
@@ -1,27 +1,53 @@
 package com.deep.escqrs
 
+import com.deep.escqrs.core.EventDescriptor
 import com.deep.escqrs.product.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.util.*
 
 class ProductTests {
+
     @Test
     fun `A product should be created`() {
+        // Arrange
         val eventStore = EventStore()
-        val repo = Repository<Product>(Product::class, eventStore)
+        val repo = Repository(Product::class, eventStore)
         val productCommandHandler = ProductCommandHandler(repo)
 
+        // Act
         val uuid = UUID.randomUUID()
         val createProduct = CreateProduct(uuid,"Bicycle", 100)
         productCommandHandler.handle(createProduct)
 
-        val changePrice = ChangeProductPrice(uuid, 10, 0)
-        productCommandHandler.handle(changePrice)
+        // Assert
+        assertEquals(
+            listOf(ProductCreated(uuid, "Bicycle", 100)),
+            eventStore.getEventsForAggregate(uuid)
+        )
+    }
 
-        val changePrice2 = ChangeProductPrice(uuid, 20, 1)
-        productCommandHandler.handle(changePrice2)
+    @Test
+    fun `Product price should be changed`() {
+        // Arrange
+        val eventStore = EventStore()
+        val repo = Repository(Product::class, eventStore)
+        val productCommandHandler = ProductCommandHandler(repo)
 
-        eventStore.printStore()
+        // Act
+        val uuid = UUID.randomUUID()
+        val createProduct = CreateProduct(uuid,"Bicycle", 100)
+        productCommandHandler.handle(createProduct)
+        val priceChange = ChangeProductPrice(uuid, 200, 0)
+        productCommandHandler.handle(priceChange)
+
+        // Assert
+        assertEquals(
+            listOf(
+                ProductPriceChanged(uuid, 200),
+                ProductCreated(uuid, "Bicycle", 100)
+            ),
+            eventStore.getEventsForAggregate(uuid)
+        )
     }
 }
