e9a8247ec6cb46d77a87eee731abb8a1d77a14c7
new20121439 <tranvandung727602@gmail.com>
2022-08-06 10:52:15 +0700

    add jpa + no-arg "to fix hibernate entity required constructor"

--- src/main/kotlin/com/deep/escqrs/core/Event.kt
+++ src/main/kotlin/com/deep/escqrs/core/Event.kt
@@ -1,9 +1,21 @@
 package com.deep.escqrs.core
 
+import org.hibernate.annotations.Type
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+
 abstract class Event : Message()
 
+@Entity
 data class EventDescriptor (
+    @Id
     val id: AggregateIdType,
+
+    @Type(type = "json")
+    @Column(nullable = false)
     val data: Event,
+
+    @Column(nullable = false)
     val version: Int
 )
